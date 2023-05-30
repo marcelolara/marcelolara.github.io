3e02849de5ceb4c6871dc43389cbd998eba25614
Marcelo Lara <chelop@gmail.com>
2023-05-29 23:28:15 -0400

    Improving index page and updating image content

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,6 +2,6 @@ export default {
   logo: <span style={{ fontWeight: 800 }}>Marcelo Lara</span>,
   project: {
     link: 'https://github.com/marcelolara'
-  }
-  // ...
+  },
+  docsRepositoryBase: 'https://github.com/marcelolara/blob/main/docs/pages'
 }
